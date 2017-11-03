768b633ac36aab29f954656b40c7eeeca1eb721e
mattgoldman93 <mattgoldman93@gmail.com>
2017-11-03 11:17:03 -0400

    Fix bug with #link-to helper
    Deletes post controller to fix issue with #link-to not routing correctly

--- app/routes/post.js
+++ app/routes/post.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
    return this.get('store').findRecord('post', params.post_id);
+   console.log(params)
  },
  actions: {
    deletePost(){
@@ -11,5 +12,14 @@ export default Ember.Route.extend({
          deleteThis.destroyRecord();
          this.transitionTo('posts');
        }
+     },
+     update(post, params) {
+       Object.keys(params).forEach(function(key) {
+       if(params[key]!==undefined) {
+         post.set(key,params[key]);
+         }
+       });
+       post.save();
+       this.transitionTo('posts');
      }
 });
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
   },
     update(post, params) {
       Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
+      if(params[key]!== undefined) {
         post.set(key,params[key]);
         }
       });
